ce756ede37d8f806d82e5606e27d0f0821ea93cd
danny <noreference@web.de>
2018-05-08 07:17:24 +0200

    Update EventService.java

--- src/org/omnirom/omnigears/service/EventService.java
+++ src/org/omnirom/omnigears/service/EventService.java
@@ -14,86 +14,312 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
-*/
+ */
 package org.omnirom.omnigears.service;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+import android.app.ActivityManagerNative;
 import android.app.Service;
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothProfile;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.location.LocationManager;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
-import android.net.ConnectivityManager;
-import android.os.BatteryManager;
-import android.os.HandlerThread;
+import android.media.AudioSystem;
+import android.media.IAudioService;
+import android.media.session.MediaSessionLegacyHelper;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
-import android.provider.Settings;
+import android.os.ServiceManager;
+import android.os.SystemClock;
+import android.os.UserHandle;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Interpolator;
+import android.view.animation.PathInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.omnirom.omnigears.R;
+
+import java.util.Set;
+
 
 public class EventService extends Service {
     private static final String TAG = "OmniEventService";
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
     private PowerManager.WakeLock mWakeLock;
+    private static boolean mIsRunning;
+    private static boolean mWiredHeadsetConnected;
+    private static boolean mA2DPConnected;
+    private static final int ANIM_DURATION = 300;
+    private static final int LEFT = 0;
+    private static final int RIGHT = 1;
+    private static final Interpolator FAST_OUT_SLOW_IN = new PathInterpolator(0.4f, 0f, 0.2f, 1f);
+    private static boolean mOverlayShown;
+    private static long mLastUnplugEventTimestamp;
+
+    private WindowManager mWindowManager;
+    private View mFloatingWidget = null;
+    private Set<String> appList = null;
+    private Handler mHandler = new Handler();
+    private PackageManager mPm;
+    private int chooserPosition;
+    private int mOverlayWidth;
+    private boolean mRecalcOverlayWidth;
+    private Runnable mCloseRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (mOverlayShown) {
+                slideAnimation(false);
+            }
+        }
+    };
 
     private BroadcastReceiver mStateListener = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             mWakeLock.acquire();
+
             try {
                 if (DEBUG) Log.d(TAG, "onReceive " + action);
-                if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
-                    if(intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1) == BluetoothAdapter.STATE_OFF){
-                    } else {
-                    }
+
+                boolean disableIfMusicActive = getPrefs(context).getBoolean(EventServiceSettings.EVENT_MUSIC_ACTIVE, true);
+                boolean autoRun = getPrefs(context).getBoolean(EventServiceSettings.EVENT_AUTORUN_SINGLE, true);
+
+                switch (action) {
+                    case BluetoothAdapter.ACTION_STATE_CHANGED:
+                        if (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1) == BluetoothAdapter.STATE_OFF) {
+                            mA2DPConnected = false;
+                        }
+                        break;
+                    case BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED:
+                        int state = intent.getIntExtra(BluetoothProfile.EXTRA_STATE,
+                                BluetoothProfile.STATE_CONNECTED);
+                        if (state == BluetoothProfile.STATE_CONNECTED && !mA2DPConnected) {
+                            mA2DPConnected = true;
+                            if (DEBUG) Log.d(TAG, "BluetoothProfile.STATE_CONNECTED = true");
+
+                            if (!(disableIfMusicActive && isMusicActive())) {
+                                appList = getPrefs(context).getStringSet(EventServiceSettings.EVENT_A2DP_CONNECT, null);
+                                if (appList != null) {
+                                    if (autoRun && appList.size() == 1) {
+                                        openApp(appList.iterator().next(), context);
+                                    } else {
+                                        openAppChooserDialog(context);
+                                    }
+                                }
+                            }
+                        } else {
+                            mA2DPConnected = false;
+                            if (DEBUG) Log.d(TAG, "BluetoothProfile.STATE_CONNECTED = false");
+                        }
+                        break;
+                    case AudioManager.ACTION_HEADSET_PLUG:
+                        boolean useHeadset = intent.getIntExtra("state", 0) == 1;
+                        final int threshold = getPrefs(context).getInt(EventServiceSettings.WIRED_EVENTS_THRESHOLD, 0);
+
+                        if (useHeadset && !mWiredHeadsetConnected) {
+                            if (mLastUnplugEventTimestamp != 0) {
+                                final long eventDelta = System.currentTimeMillis() - mLastUnplugEventTimestamp;
+                                if (eventDelta < threshold * 1000) {
+                                    if (DEBUG) Log.d(TAG, "Ignore AudioManager.ACTION_HEADSET_PLUG = " + useHeadset + " delta = " + eventDelta);
+                                    return;
+                                }
+                            }
+                            mWiredHeadsetConnected = true;
+                            if (DEBUG) Log.d(TAG, "AudioManager.ACTION_HEADSET_PLUG = true");
+
+                            if (!(disableIfMusicActive && isMusicActive())) {
+                                appList = getPrefs(context).getStringSet(EventServiceSettings.EVENT_WIRED_HEADSET_CONNECT, null);
+                                if (appList != null) {
+                                    if (autoRun && appList.size() == 1) {
+                                        openApp(appList.iterator().next(), context);
+                                    } else {
+                                        openAppChooserDialog(context);
+                                    }
+                                }
+                            }
+                        } else {
+                            mWiredHeadsetConnected = false;
+                            if (DEBUG) Log.d(TAG, "AudioManager.ACTION_HEADSET_PLUG = false");
+                            mLastUnplugEventTimestamp = System.currentTimeMillis();
+                        }
+                        break;
                 }
-                if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
-                }
-                if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
-                }
-                if (BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED.equals(action)) {
-                    int state = intent.getIntExtra(BluetoothProfile.EXTRA_STATE,
-                            BluetoothProfile.STATE_CONNECTED);
-                    if (state == BluetoothProfile.STATE_CONNECTED) {
-                        if (DEBUG) Log.d(TAG, "BluetoothProfile.STATE_CONNECTED = true" );
-                    } else {
-                        if (DEBUG) Log.d(TAG, "BluetoothProfile.STATE_CONNECTED = false" );
-                    }
-                }
-                if (AudioManager.ACTION_HEADSET_PLUG.equals(action)) {
-                    boolean useHeadset = intent.getIntExtra("state", 0) == 1;
-                    if (useHeadset) {
-                        if (DEBUG) Log.d(TAG, "AudioManager.ACTION_HEADSET_PLUG = true" );
-                    } else {
-                        if (DEBUG) Log.d(TAG, "AudioManager.ACTION_HEADSET_PLUG = false" );
-                    }
-                }
-                
+
             } finally {
                 mWakeLock.release();
             }
         }
     };
 
+    public void openAppChooserDialog(final Context context) {
+        if (!mOverlayShown) {
+            if (mRecalcOverlayWidth) {
+                mOverlayWidth = getOverlayWidth(context);
+                mRecalcOverlayWidth = false;
+            }
+            // never enter again once this started
+            mOverlayShown = true;
+
+            final LayoutInflater inflater = LayoutInflater.from(new ContextThemeWrapper(
+                context, android.R.style.Theme_DeviceDefault_Light_Dialog));
+            mFloatingWidget = inflater.inflate(R.layout.layout_floating_widget, null);
+
+            final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_PHONE,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSLUCENT);
+
+            params.x = context.getResources().getDimensionPixelSize(R.dimen.floating_widget_window_padding);
+
+            // Selected apps
+            LinearLayout linearLayout = (LinearLayout) mFloatingWidget.findViewById(R.id.selected_apps);
+            if (linearLayout.getChildCount() > 0) linearLayout.removeAllViews();
+
+            for (final String value : appList) {
+                try {
+                    View v = inflater.inflate(R.layout.app_grid_item, null);
+                    ComponentName componentName = ComponentName.unflattenFromString(value);
+                    Drawable icon = mPm.getActivityIcon(componentName);
+                    ((ImageView) v.findViewById(R.id.appIcon)).setImageDrawable(icon);
+                    v.setPadding(30, 15, 30, 15);
+                    v.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            mHandler.removeCallbacks(mCloseRunnable);
+                            mOverlayShown = false;
+                            try {
+                                mWindowManager.removeViewImmediate(mFloatingWidget);
+                            } catch (Exception e) {
+                                Log.e(TAG, "openApp ", e);
+                            }
+                            openApp(value, context);
+                        }
+                    });
+                    linearLayout.addView(v);
+                } catch (PackageManager.NameNotFoundException e) {
+                    Log.e(TAG, "Set app icon", e);
+                }
+            }
+
+            // Close button
+            View close = inflater.inflate(R.layout.app_grid_item, null);
+            close.setPadding(30, 15, 30, 15);
+            close.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mHandler.removeCallbacks(mCloseRunnable);
+                    slideAnimation(false);
+                }
+            });
+
+            // Position and close icon
+            chooserPosition = getPrefs(context).getInt(EventServiceSettings.APP_CHOOSER_POSITION, LEFT);
+            if (chooserPosition == LEFT) {
+                params.gravity = Gravity.CENTER_VERTICAL | Gravity.LEFT;
+                ((ImageView) close.findViewById(R.id.appIcon)).setImageResource(R.drawable.ic_chevron_left);
+            } else {
+                params.gravity = Gravity.CENTER_VERTICAL | Gravity.RIGHT;
+                ((ImageView) close.findViewById(R.id.appIcon)).setImageResource(R.drawable.ic_chevron_right);
+            }
+
+            linearLayout.addView(close);
+
+            mWindowManager.addView(mFloatingWidget, params);
+            slideAnimation(true);
+
+            final int timeout = getPrefs(context).getInt(EventServiceSettings.APP_CHOOSER_TIMEOUT, 15);
+            if (timeout > 0) {
+                mHandler.postDelayed(mCloseRunnable, timeout * 1000);
+            }
+        }
+    }
+
+    private void openApp(String app_uri, Context context) {
+        try {
+            startActivityAsUser(createIntent(app_uri), UserHandle.CURRENT);
+            if (getPrefs(context).getBoolean(EventServiceSettings.EVENT_MEDIA_PLAYER_START, false)) {
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        dispatchMediaKeyToAudioService(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
+                    }
+                }, 1000);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "MultiAppSelector.EVENT_MEDIA_PLAYER_START", e);
+        }
+    }
+
+    private void dispatchMediaKeyToAudioService(int keycode) {
+        if (ActivityManagerNative.isSystemReady()) {
+            IAudioService audioService = IAudioService.Stub
+                    .asInterface(ServiceManager.checkService(Context.AUDIO_SERVICE));
+            if (audioService != null) {
+                if (DEBUG) Log.d(TAG, "dispatchMediaKeyToAudioService " + keycode);
+
+                KeyEvent event = new KeyEvent(SystemClock.uptimeMillis(),
+                        SystemClock.uptimeMillis(), KeyEvent.ACTION_DOWN,
+                        keycode, 0);
+                MediaSessionLegacyHelper.getHelper(this).sendMediaButtonEvent(event, true);
+                event = KeyEvent.changeAction(event, KeyEvent.ACTION_UP);
+                MediaSessionLegacyHelper.getHelper(this).sendMediaButtonEvent(event, true);
+            }
+        }
+    }
+
+    private Intent createIntent(String value) {
+        ComponentName componentName = ComponentName.unflattenFromString(value);
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+        intent.setComponent(componentName);
+        return intent;
+    }
+
+    private boolean isMusicActive() {
+        if (AudioSystem.isStreamActive(AudioSystem.STREAM_MUSIC, 0)) {
+            // local / wired / BT playback active
+            if (DEBUG) Log.d(TAG, "isMusicActive(): local");
+            return true;
+        }
+        if (AudioSystem.isStreamActiveRemotely(AudioSystem.STREAM_MUSIC, 0)) {
+            // remote submix playback active
+            if (DEBUG) Log.d(TAG, "isMusicActive(): remote submix");
+            return true;
+        }
+        if (DEBUG) Log.d(TAG, "isMusicActive(): no");
+        return false;
+    }
+
     public class LocalBinder extends Binder {
         public EventService getService() {
             return EventService.this;
         }
     }
+
     private final LocalBinder mBinder = new LocalBinder();
 
     @Override
@@ -103,8 +329,17 @@ public class EventService extends Service {
         PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
         mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
         mWakeLock.setReferenceCounted(true);
-
+        mIsRunning = true;
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mPm = getPackageManager();
         registerListener();
+        mOverlayWidth = getOverlayWidth(this);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mRecalcOverlayWidth = true;
     }
 
     @Override
@@ -117,6 +352,7 @@ public class EventService extends Service {
         super.onDestroy();
         if (DEBUG) Log.d(TAG, "onDestroy");
         unregisterListener();
+        mIsRunning = false;
     }
 
     private void registerListener() {
@@ -135,6 +371,63 @@ public class EventService extends Service {
         try {
             this.unregisterReceiver(mStateListener);
         } catch (Exception e) {
+            Log.e(TAG, "unregisterListener", e);
+        }
+    }
+
+    public static boolean isRunning() {
+        return mIsRunning;
+    }
+
+    private SharedPreferences getPrefs(Context context) {
+        return context.getSharedPreferences(EventServiceSettings.EVENTS_PREFERENCES_NAME, Context.MODE_PRIVATE);
+    }
+
+    private int getOverlayWidth(Context context) {
+        return (context.getResources().getDimensionPixelSize(R.dimen.floating_widget_view_padding) +
+                context.getResources().getDimensionPixelSize(android.R.dimen.app_icon_size)) / 2;
+    }
+
+    private void slideAnimation(final boolean show) {
+        if (show) {
+            int startValue = 0;
+            if (chooserPosition == RIGHT) {
+                startValue = mOverlayWidth;
+            } else {
+                startValue = -mOverlayWidth;
+            }
+            mFloatingWidget.setTranslationX(startValue);
+            mFloatingWidget.setAlpha(0);
+            mFloatingWidget.animate()
+                    .alpha(1)
+                    .translationX(0)
+                    .setDuration(ANIM_DURATION)
+                    .setInterpolator(FAST_OUT_SLOW_IN)
+                    .start();
+
+        } else {
+            int endValue = 0;
+            if (chooserPosition == RIGHT) {
+                endValue = mOverlayWidth;
+            } else {
+                endValue = -mOverlayWidth;
+            }
+            mFloatingWidget.setTranslationX(0);
+            mFloatingWidget.setAlpha(1);
+            mFloatingWidget.animate()
+                    .alpha(0)
+                    .translationX(endValue)
+                    .setDuration(ANIM_DURATION)
+                    .setInterpolator(FAST_OUT_SLOW_IN)
+                    .withEndAction(() -> {
+                        mOverlayShown = false;
+                        try {
+                            mWindowManager.removeViewImmediate(mFloatingWidget);
+                        } catch (Exception e) {
+                            Log.e(TAG, "slideAnimation close ", e);
+                        }
+                    })
+                    .start();
         }
     }
 }
